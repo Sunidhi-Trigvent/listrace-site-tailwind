8617aacabe69f509b6a8a0822138d248c0e8b9e6
Sunidhi <sunidhi@trigvent.com>
2024-09-30 12:11:16 +0530

    First commit

---
+++ src/views/layout/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./header";
+import Home from "../pages/Home";
+
+function MainLayout() {
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  );
+}
+
+export default MainLayout;
