fd969dabfc3855ad32b680a301085789acd4da15
Sunidhi <sunidhi@trigvent.com>
2024-10-01 12:56:22 +0530

    commit

--- src/views/layout/header/toparea.jsx
+++ src/views/layout/header/toparea.jsx
@@ -3,27 +3,27 @@ import React from "react";
 function Toparea() {
   return (
     <header className="bg-slate-300 py-2 lg:py-0">
-      <div className="flex flex-col items-center p-2 lg:h-10 lg:w-full lg:flex-row lg:justify-between">
+      <div className="flex flex-col items-start p-2 lg:h-10 lg:w-full lg:flex-row lg:justify-between">
         {/* Language and Currency Selection */}
-        <div className="flex flex-col items-center space-y-2 sm:flex-row sm:space-x-2 sm:space-y-0 lg:space-x-4">
-          <select className="mx-1 rounded border p-1">
+        <div className="flex items-center space-x-2 lg:space-x-4">
+          <select className="rounded border">
             <option value="EN">EN</option>
             <option value="BN">BN</option>
             <option value="AB">AB</option>
           </select>
-          <select className="mx-1 rounded border p-1">
+          <select className="rounded border">
             <option value="USD">USD</option>
             <option value="EURO">EURO</option>
             <option value="BDT">BDT</option>
           </select>
-          <span className="mx-1">src</span>
+          <span>src</span>
         </div>
 
         {/* Contact Info and Auth Links */}
-        <div className="mt-2 flex flex-col items-center space-y-2 sm:flex-row sm:space-x-4 sm:space-y-0 md:mt-0">
-          <span className="mx-1">+1 222 777 6565</span>
-          <span className="mx-1">Sign In</span>
-          <span className="mx-1">Register</span>
+        <div className="mt-2 flex items-center space-x-4 md:mt-0 lg:mt-0">
+          <span>+1 222 777 6565</span>
+          <span>Sign In</span>
+          <span>Register</span>
         </div>
       </div>
     </header>
